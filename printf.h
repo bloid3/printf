92fc52682539d837651ac370d6b7790327b4eb97
bloid3 <pablo.pereira00@gmail.com>
2022-10-18 18:49:37 +0200

    1810

--- printf.h
+++ printf.h
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/18 16:50:18 by papereir          #+#    #+#             */
-/*   Updated: 2022/10/18 18:49:16 by papereir         ###   ########.fr       */
+/*   Created: 2022/09/01 12:55:46 by papereir          #+#    #+#             */
+/*   Updated: 2022/10/18 18:23:23 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#ifndef PRINTF_H
+# define PRINTF_H
 
-int	ft_printf(char const *format, ...)
-{
-	va_list	args;
-	
-	va_start(args, format);
-	
-}
+# include <string.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+
+int		ft_printf(char	*args ,...);
+
+#endif
